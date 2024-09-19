4dfde75667b7415acea0ecd9b13c72e5a5ce1696
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2024-09-19 19:17:14 -0400

    Speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import HomePage from './screens/HomePage.jsx'
 import NBA_API from './screens/NBA-API';
 
