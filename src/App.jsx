d34e29487ae6404f06a1f2e5aecd1056520faff6
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2024-11-18 11:38:09 -0500

    Google analytics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Analytics } from "@vercel/analytics/react";
+import { ReactGA } from 'react-ga4';
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import HomePage from './screens/HomePage.jsx';
 import NBA_API from './screens/NBA-API/NBA-API';
@@ -9,6 +10,13 @@ import { GraphDataProvider } from './screens/LockedIn/GraphDataContext'; // Impo
 
 function App() {
   useEffect(() => {
+
+    const location = useLocation();
+
+    // Initialize Google Analytics
+    ReactGA.initialize('G-62M60FY37N');
+    ReactGA.send({ hitType: 'pageview', page: location.pathname, title: document.title });
+
     // POST request to spin up the API
     fetch('https://nba-statistics-visualizer.onrender.com/submit', {
       method: 'POST',
@@ -26,7 +34,7 @@ function App() {
       .catch((error) => {
         console.error('Error:', error);
       });
-  }, []);
+  }, [location]);
 
   return (
     <div className="App">
